209233ea8c5d6081b07c7435f448e9b62ffc8999
Deepak <deepaksolanki9759@gmail.com>
2025-04-18 18:22:54 +0530

    first commit

---
+++ src/main/java/com/deepak/gcp/backend/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.deepak.gcp.backend.controller;
+
+import com.deepak.gcp.backend.dto.ContentDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hi")
+    public ResponseEntity<ContentDto> hi(){
+        return ResponseEntity.ok(new ContentDto("Hi, Khana kb khana h?"));
+    }
+}
---
+++ src/main/java/com/deepak/gcp/backend/dto/ContentDto.java
@@ -0,0 +1,5 @@
+package com.deepak.gcp.backend.dto;
+
+public record ContentDto(String content) {
+
+}
